7fa35844ca8b4364710e1442a1bb3238c65c65c7
David Hong <daviddhongg@gmail.com>
2019-12-03 17:30:26 -0800

    JS | AJAX - David Hong

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -1,5 +1,5 @@
 //Create GLOBAL variable below here on line 2
-
+var global_result;
 
 
 $(document).ready(initializeApp);
@@ -14,9 +14,19 @@ function getData(){
         dataType: 'json',
         url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
         success: function(result) {
-            
-            console.log('2) AJAX Success function called, with the following result:', result);
 
+            console.log('2) AJAX Success function called, with the following result:', result);
+            global_result = result;
+
+            var movie0PictureInfo = result.feed.entry[0]["im:image"][2].label;
+
+            for(var i=0; i<10; i++){
+                var imageUrl = result.feed.entry[i]["im:image"][2].label;
+                var movieTitle = result.feed.entry[i].title.label;
+                var movieDirector = result.feed.entry[i]["im:artist"].label;
+                var divCard = $("<div>").append($('<img>').attr('src', imageUrl), $("<p>").text(movieTitle), $("<p>").text(movieDirector))
+                $("#main").append(divCard);
+            }
         }
     }
 
